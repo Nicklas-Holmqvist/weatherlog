8e0ece10e964163d7ea96613d61e25408dc44560
nicklas-holmqvist <nicklas_holmqvist@outlook.comgit config --global user.email nicklas_holmqvist@outlook.com>
2021-12-08 07:08:03 +0100

    added mongoDb connection

--- server.js
+++ server.js
@@ -3,18 +3,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require("mongoose");
 
 // Create a new express application named 'app'
 const app = express();
 
 // Set our backend port to be either an environment variable or port 5000
 const port = process.env.PORT || 5000;
+const mognoDb = 'mongodb+srv://nicklas:!Nicklas@weatherlog.3tpno.mongodb.net/weatherlog'
 
 // This application level middleware prints incoming requests to the servers console
 app.use((req, res, next) => {
     console.log(`Request_Endpoint: ${req.method} ${req.url}`);
     next();
 });
+
+async function run() {
+    try {
+      await mongoose.connect(
+        mognoDb,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      );
+      console.log("Database is connected");
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 // Configure the bodyParser middleware
 app.use(bodyParser.json());
@@ -49,3 +66,5 @@ app.get('*', (req, res) => {
 
 // Configure our server to listen on the port defiend by our port variable
 app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
+
+run()
