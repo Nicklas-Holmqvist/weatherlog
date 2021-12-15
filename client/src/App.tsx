0a1a3c32bc4c963b7166c51f23c0d87ddabee8e0
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2021-12-15 16:09:54 +0100

    merge main with main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Typography } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 import './App.css';
@@ -8,34 +8,9 @@ import theme from './theme';
 import routes from './routes';
 
 function App() {
-	const [api, setApi] = useState<string>('');
-
-	useEffect(() => {
-		const options = {
-			method: 'get',
-		};
-		const fetchApi = async () => {
-			await fetch('/api/hello-server', options)
-				.then(function (res) {
-					if (res.status === 400) {
-						return;
-					}
-					return res.json();
-				})
-				.then(function (data) {
-					setApi(data);
-				})
-				.catch(function (err) {
-					console.error(err);
-				});
-		};
-
-		fetchApi();
-	});
 
 	return (
 		<ThemeProvider theme={theme}>
-			<Typography variant="h2">{api}</Typography>
 			<BrowserRouter>
 				<Routes>
 					{routes.map(({ path, element }, key) => (
