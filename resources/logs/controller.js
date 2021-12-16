189414cb6981679a3cdfa48d9639ef85eebedcd0
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2021-12-16 16:28:22 +0100

    creating log api

--- resources/logs/controller.js
+++ resources/logs/controller.js
@@ -11,3 +11,130 @@ exports.getLogs = async (req, res) => {
         res.status(503).json('No database connection')
     }       
 }
+
+exports.createLog = async (req, res) => {
+    const { 
+        airFeeling,
+        airpressure,
+        date,
+        description,
+        humidity,
+        precipitation,
+        temperature,
+        windDirection,
+        weather,
+        windSpeed
+    } = req.body
+    
+    const userId = "61b9d2dcbd5038e17fc353a3"
+    const dateExist = await LogModel.exists({ date: date });
+    
+    const newLog = {
+        airFeeling: airFeeling,
+        airpressure: airpressure,
+        date: date,
+        description: description,
+        humidity: humidity,
+        precipitation: precipitation,
+        temperature: temperature,
+        user: userId,
+        windDirection: windDirection,
+        weather: weather,
+      windSpeed: windSpeed,
+    }
+    
+    if (!dateExist) {     
+        try {
+            const log = await LogModel.create(newLog)
+            res.status(201).json(log)
+        console.log({log: newLog})
+    } catch (error) {
+        res.status(400).json(error)
+    }
+    } else {
+        let errors = { msg: '' }
+        
+        // if email already exists in db
+        errors.msg = 'Datumet är redan loggat!'
+        
+        res.status(400).json({ errors })
+    }
+}
+
+exports.changeLog = async (req, res) => {
+    const log = req.params.id
+
+    const { 
+        airFeeling,
+        airpressure,
+        date,
+        description,
+        humidity,
+        precipitation,
+        temperature,
+        windDirection,
+        weather,
+        windSpeed
+    } = req.body
+
+    const getLog = await LogModel.findById(log);
+    
+    const userId = "61b9d2dcbd5038e17fc353a3"
+    // const userId = ""
+    // const userExist = await LogModel.exists({ user: userId });
+    
+    const newLog = {
+        airFeeling: airFeeling,
+        airpressure: airpressure,
+        date: date,
+        description: description,
+        humidity: humidity,
+        precipitation: precipitation,
+        temperature: temperature,
+        user: userId,
+        windDirection: windDirection,
+        weather: weather,
+        windSpeed: windSpeed,
+    }
+
+    if (getLog && userId !== "") {     
+        try {
+            if(getLog) {
+                await LogModel.findByIdAndUpdate({ _id: log }, newLog)
+                res.status(200).json('Log has been updated!')
+                console.log(newLog)
+            } else {
+                res.status(400).json('No log!')
+            }
+        } catch (error) {
+            res.status(400).json(error)
+    }
+    } else {
+        let errors = { msg: '' }
+        
+        // if email already exists in db
+        errors.msg = 'No log to update!'
+        
+        res.status(400).json({ errors })
+    }
+}
+
+exports.deleteLog = async (req, res) => {
+    const log = req.params.id
+    const userId = "61b9d2dcbd5038e17fc353a3"
+    // const userId = ""
+    const getLog = await LogModel.findById(log);
+    if (getLog && userId) {     
+        try {
+            await LogModel.findByIdAndRemove({ _id: log })
+            res.status(201).json(log)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+    } else {
+        let errors = { msg: '' }        
+        // if email already exists in db
+        errors.msg = 'Not autherize to delete this log'        
+        res.status(400).json({ errors })
+    }
+}
--- resources/logs/router.js
+++ resources/logs/router.js
@@ -3,5 +3,8 @@ const controller = require('./controller')
 const router = express.Router();
 
 router.get("/api/logs/", controller.getLogs);
+router.post("/api/logs/register", controller.createLog);
+router.put("/api/logs/:id", controller.changeLog);
+router.delete("/api/logs/:id", controller.deleteLog);
 
 module.exports = router;
