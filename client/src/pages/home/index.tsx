3f53f12a4c420e3daaca6c85c0c5073af37c2119
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2021-12-21 11:17:50 +0100

    all user api is working

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,9 +1,19 @@
 import { Grid, Typography } from '@material-ui/core';
+import { useUsersContext } from '../../context/users';
 
 export const HomePage = () => {
+
+	const user = useUsersContext()
+
 	return (
 		<Grid container>
 			<Typography variant="h4">Startsidan</Typography>
+			<button onClick={user.addUser}>Skapa konto</button>
+			<button onClick={user.addUserInfo}>Lägg till info</button>
+			<button onClick={user.editUser}>Edit user</button>
+			<button onClick={user.changePassword}>Ändra lösenord</button>
+			<button onClick={user.logout}>Logout</button>
+			<button onClick={user.deleteUser}>Ta bort konto</button>
 		</Grid>
 	);
 };
