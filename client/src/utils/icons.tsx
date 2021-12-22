955b678993105fc2a4d4dc51f77399825d6932d4
Oliver Nygren <oliver.nygren@icloud.com>
2021-12-22 14:17:47 +0100

    arrow icons and switch rain icons

--- client/src/utils/icons.tsx
+++ client/src/utils/icons.tsx
@@ -10,6 +10,15 @@ import snowyRain from '../icons/snowy-rain.svg';
 import sun from '../icons/sun.svg';
 import thunder from '../icons/thunder.svg';
 
+import n from '../icons/n.svg';
+import nw from '../icons/nw.svg';
+import w from '../icons/w.svg';
+import sw from '../icons/sw.svg';
+import s from '../icons/s.svg';
+import se from '../icons/se.svg';
+import e from '../icons/e.svg';
+import ne from '../icons/ne.svg';
+
 interface IIcon {
 	className?: string;
 }
@@ -58,4 +67,36 @@ export const Sun = ({ className }: IIcon) => {
 
 export const Thunder = ({ className }: IIcon) => {
 	return <img className={className} src={thunder} alt="Åska"></img>;
+};
+
+export const North = ({ className }: IIcon) => {
+	return <img className={className} src={n} alt="Nordlig"></img>;
+};
+
+export const NorthWest = ({ className }: IIcon) => {
+	return <img className={className} src={nw} alt="Nordvästlig"></img>;
+};
+
+export const West = ({ className }: IIcon) => {
+	return <img className={className} src={w} alt="Västlig"></img>;
+};
+
+export const SouthWest = ({ className }: IIcon) => {
+	return <img className={className} src={sw} alt="Sydvästlig"></img>;
+};
+
+export const South = ({ className }: IIcon) => {
+	return <img className={className} src={s} alt="Sydlig"></img>;
+};
+
+export const SouthEast = ({ className }: IIcon) => {
+	return <img className={className} src={se} alt="Sydöstlig"></img>;
+};
+
+export const East = ({ className }: IIcon) => {
+	return <img className={className} src={e} alt="Östlig"></img>;
+};
+
+export const NorthEast = ({ className }: IIcon) => {
+	return <img className={className} src={ne} alt="Nordöstlig"></img>;
 };
