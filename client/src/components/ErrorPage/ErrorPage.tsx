c5e95817218305157ff2a432902fc486e9632a65
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-01-10 15:57:32 +0100

    done with error design

--- client/src/components/ErrorPage/ErrorPage.tsx
+++ client/src/components/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { Button, Typography } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
+
+import {Error} from '../../utils/icons'
+import useStyles from './styles';
 
 export const ErrorPage = () => {
-    return <h1>Du har hamlat lite fel!</h1>
+
+    const navigateTo = useNavigate()
+
+    const classes = useStyles()
+    const handleClick = () => {
+        navigateTo('/');        
+    }
+
+    return (
+        <Grid container className={classes.container}>
+            <Grid item direction='column' className={classes.content}>
+                <Error />   
+                <Typography variant="h4" className={classes.text}>
+					Ett oväder drog in...
+				</Typography>
+                <Button onClick={handleClick}>Gå till start</Button>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default ErrorPage
