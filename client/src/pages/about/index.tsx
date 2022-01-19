3bdd81acd1a48235bc25e102ca191709b8245879
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-01-19 14:38:57 +0100

    done

--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -8,46 +8,47 @@ export const AboutPage = () => {
 	const classes = useStyles();
 
 	return (
-		<Grid container item direction="column" className={classes.root}>
-			<Grid item container className={classes.titleContainer}>
-				<NavigateBackButton page="back" />
-				<Typography variant="h2" className={classes.title}>
-					Om
-				</Typography>
-			</Grid>
-			<Grid item className={classes.textBodyContainer}>
-				<Typography variant="body1">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-					quisquam ratione quae blanditiis iste incidunt. Ipsum quis ratione
-					repellendus culpa illum maxime laboriosam earum fugit officia, a
-					laudantium blanditiis soluta. Ipsum quis ratione repellendus culpa
-					illum maxime laboriosam earum fugit officia, a laudantium blanditiis
-					soluta.
-				</Typography>
-				<img src={logo} alt="Logo" className={classes.logo} />
-			</Grid>
-			<Grid item>
-				<Typography variant="h5" className={classes.subtitle}>
-					Rubrik
-				</Typography>
-				<Typography variant="body1">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-					quisquam ratione quae blanditiis iste incidunt. Ipsum quis ratione
-					repellendus culpa illum maxime laboriosam earum fugit officia, a
-					laudantium blanditiis soluta.
-				</Typography>
-			</Grid>
-			<Grid item>
-				<Typography variant="h5" className={classes.subtitle}>
-					Rättigheter
-				</Typography>
-				<Typography variant="subtitle1">
-					Copyright © 2022 | Väderdagboken
-				</Typography>
-				<Typography variant="body1">Alla rättigheter reserverade</Typography>
-			</Grid>
-		</Grid>
-	);
+    <Grid container item direction="column" className={classes.root}>
+      <Grid item container className={classes.titleContainer}>
+        <NavigateBackButton page="back" />
+        <Typography variant="h2" className={classes.title}>
+          Om
+        </Typography>
+      </Grid>
+      <Grid item className={classes.textBodyContainer}>
+        <Typography variant="body1">
+          Väderdagboken är en digital dagbok för att logga vädret hos dig. Du
+          kan med Väderdagboken logga dagar, ändra och ta bort dem. Se
+          historiken per månad i diagram-form och listvy men även alla loggar i
+          en enda list-vy. Det är inte bara personer som kan använda den här
+          webb-appen utan även skolklasser som vill se hur vädret förändras inom
+          en viss period. Alla som är intresserade av vädret!
+        </Typography>
+        <img src={logo} alt="Logo" className={classes.logo} />
+      </Grid>
+      <Grid item>
+        <Typography variant="h5" className={classes.subtitle}>
+          Idé
+        </Typography>
+        <Typography variant="body1">
+          Skaparna är Nicklas Holmqvist och Oliver Nygren som ett examensarbete
+          i Frontendutbildningen 2022 på Medieinstitutet i Göteborg. Grundidén
+          till Väderdagboken var något som Nicklas hade med sig som barn då han
+          satt och skapade egna diagram i ett rut-block. Nu kan man logga det
+          mesta som en väderstation kan visa.
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="h5" className={classes.subtitle}>
+          Rättigheter
+        </Typography>
+        <Typography variant="subtitle1">
+          Copyright © 2022 | Väderdagboken
+        </Typography>
+        <Typography variant="body1">Alla rättigheter reserverade</Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default AboutPage;
