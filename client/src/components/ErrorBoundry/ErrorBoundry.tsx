6788e1e08cb2a7fdb34258d2675457c56792471d
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-01-26 12:56:34 +0100

    fixed error boundry with location.replace

--- client/src/components/ErrorBoundry/ErrorBoundry.tsx
+++ client/src/components/ErrorBoundry/ErrorBoundry.tsx
@@ -1,6 +1,7 @@
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { Component, ErrorInfo, ReactNode } from 'react';
-
-import { ErrorPage } from '../ErrorPage'
+import { Helmet } from 'react-helmet-async';
 
 interface Props {
     children: ReactNode;
@@ -26,9 +27,22 @@ class ErrorBoundary extends Component<Props, State> {
         console.error("Uncaught error:", error, errorInfo);
     }
 
+    handleClick = () => {
+        window.location.replace('/')
+    }
+
     public render() {
         if (this.state.hasError) {
-            return <ErrorPage />
+            return (
+                <Grid container direction='column' justifyContent='center' alignItems='center'>
+                    <Helmet>
+                        <title>404 - Error | Väderdagboken</title>
+                        <meta name="error" content="Du har kommit lite fel" />
+                    </Helmet>
+                    <h1>Något gick fel!</h1>
+                    <Button onClick={this.handleClick}>Ladda om sidan</Button>
+                </Grid>
+            )
         }
         return this.props.children
     }
